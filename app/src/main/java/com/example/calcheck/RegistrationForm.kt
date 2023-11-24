c2db212d3c7abe122a396851966ae52b7146acd1
Allain Daniel Espina <allaindaniel.espina.cics@ust.edu.ph>
2023-11-24 11:59:25 +0700

    Added Registration and Dashboard Layouts

---
+++ app/src/main/java/com/example/calcheck/RegistrationForm.kt
@@ -0,0 +1,11 @@
+package com.example.calcheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_form)
+    }
+}
