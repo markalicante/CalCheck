80e33f930883bf3b0abbce4cfb3462f94514b2cf
Allain Daniel Espina <allaindaniel.espina.cics@ust.edu.ph>
2023-11-24 12:10:39 +0700

    Updated Activities (MainActivity and RegistrationForm)

--- app/src/main/java/com/example/calcheck/MainActivity.kt
+++ app/src/main/java/com/example/calcheck/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.calcheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -16,6 +18,18 @@ class MainActivity : AppCompatActivity() {
 
         //Declaration of Firebase DB Instance
         databaseReference = FirebaseDatabase.getInstance().getReference("DatabaseName")
+
+        var buttonLogin : Button = findViewById(R.id.btnLogin)
+
+        buttonLogin.setOnClickListener(){
+            try{
+
+
+
+            } catch(e : Exception){
+                Log.e("error_espina", e.message.toString())
+            }
+        }
 
     }
 }
--- app/src/main/java/com/example/calcheck/RegistrationForm.kt
+++ app/src/main/java/com/example/calcheck/RegistrationForm.kt
@@ -2,10 +2,28 @@ package com.example.calcheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class RegistrationForm : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_form)
+
+        var buttonRegister : Button = findViewById(R.id.btnRegister)
+
+        buttonRegister.setOnClickListener(){
+            try{
+
+
+
+            } catch(e : Exception){
+                Log.e("error_espina", e.message.toString())
+            }
+        }
+
     }
 }
