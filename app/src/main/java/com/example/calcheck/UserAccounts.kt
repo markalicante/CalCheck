29dc8134b1bc29a6974671df9948cda1c0169775
Allain Daniel Espina <allaindaniel.espina.cics@ust.edu.ph>
2023-11-24 12:43:10 +0700

    Added User Accounts Data Class (UserAccounts)

---
+++ app/src/main/java/com/example/calcheck/UserAccounts.kt
@@ -0,0 +1,5 @@
+package com.example.calcheck
+
+data class UserAccounts (var userName : String,
+                         var userPassword : String,
+                         var userTargetCal : Int)
