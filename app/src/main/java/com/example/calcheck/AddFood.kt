6036c2269999142beb79e4428333ef9b00faaacc
MARK <MARK@MARK>
2023-12-13 23:30:26 +0800

    front end done

---
+++ app/src/main/java/com/example/calcheck/AddFood.kt
@@ -0,0 +1,11 @@
+package com.example.calcheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFood : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_food)
+    }
+}
--- app/src/main/java/com/example/calcheck/FoodActivity.kt
+++ app/src/main/java/com/example/calcheck/FoodActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AbsListView.RecyclerListener
+import android.widget.Button
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class FoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food)
     }
+
+
+
 
     fun prevPage(view: View){
         val page2 = Intent(this, Dashboard::class.java)
